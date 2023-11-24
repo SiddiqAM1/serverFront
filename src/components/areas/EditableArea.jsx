301423c7a4455e900c29f52c152f6c7340a5544b
Muhammad Siddiq <ahmedmuhammadsiddiq4@gmail.com>
2023-11-24 14:57:55 +0500

    Update EditableArea.jsx

--- src/components/areas/EditableArea.jsx
+++ src/components/areas/EditableArea.jsx
@@ -13,7 +13,7 @@ const EditableArea = ({ area}) => {
     const handleConfirm = (id,name) => {
         console.log(id,name)
       // Make API call to update the name in the database
-      fetch(`https://server-front-ochre.vercel.app/area/update/${id}`, {
+      fetch(`https://server-sigma-peach.vercel.app/api/area/update/${id}`, {
         method: "PUT",
         headers: {
             "Content-Type": "application/json",
